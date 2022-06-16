d9a8b31fdb545db85dec2410426df72bce94d817
Davide Zago <dawoz.98@gmail.com>
2022-06-16 09:40:34 +0200

    Upload of train scripts

---
+++ train_scripts/run100.sh
@@ -0,0 +1,2 @@
+python run.py --graph_size 100 --run_name 'tsp100_rollout_20_epochs_BERT' --baseline rollout --n_epochs 20 --model attention_bert;
+python run.py --graph_size 100 --run_name 'tsp100_rollout_20_epochs_AM' --baseline rollout --n_epochs 20 --model attention
---
+++ train_scripts/run_tsp100_100_epochs_BERT.sh
@@ -0,0 +1,1 @@
+python run.py --graph_size 100 --run_name 'tsp100_rollout_100_epochs_BERT' --baseline rollout --n_epochs 100 --model attention_bert
