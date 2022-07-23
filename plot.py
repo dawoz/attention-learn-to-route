08e832998a8828cab0de6edcec7852c841aad610
Davide Zago <dawoz.98@gmail.com>
2022-07-23 12:11:22 +0200

    Update

--- plot.py
+++ plot.py
@@ -1,20 +1,32 @@
 from tensorboard.backend.event_processing import event_accumulator
-from pprint import pprint
+from pprint import pp, pprint
 from sys import argv
 import matplotlib.pyplot as plt
+import glob
 
 if __name__ == '__main__':
     if len(argv) != 2:
-        print('Usage: plot.py <log_file>')
+        print('Usage: plot.py <log_dir>')
         exit(1)
 
-    ea = event_accumulator.EventAccumulator(argv[1])
+    
+    logs = glob.glob(argv[1] + '/**/events.out.*', recursive=True)
 
-    ea.Reload()  # loads events from file
+    eas = [event_accumulator.EventAccumulator(l) for l in logs]
+    names = [l.replace('\\','/').split('/')[-2] for l in logs]
 
-    for serie_name in ea.Tags()['scalars']:
-        serie = [e.value for e in ea.Scalars(serie_name)]
-        
-        plt.plot(serie, label=serie_name)
+    for ea in eas:
+        ea.Reload()
+
+    #pprint(eas[0].Tags()['scalars'])
+
+    for serie_name in ['avg_cost', 'actor_loss', 'nll', 'grad_norm', 'val_avg_reward']:
+        plt.figure(dpi=100)
+
+        for i, ea in enumerate(eas):    
+            serie = [e.value for e in ea.Scalars(serie_name)]
+            plt.plot(serie, label=names[i])
+
         plt.legend()
+        plt.title(serie_name)
         plt.show()
